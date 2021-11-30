e2291ac16809bc0cb87f1176807490ffc75d489d
Ben Caulfield <ben.caulfield@outlook.com>
2021-11-30 16:23:53 +0000

    Initial Commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const Tail = require('tail').Tail;
+const fs = require('fs');
+
+recordedips = []
+
+config = {
+    booterName: "freebooter.xonos", // A unique name for the attack (used for logging + file names)
+    logDirectory: "/var/log/nginx/access.log"
+}
+
+const tail = new Tail('/var/log/nginx/access.log');
+
+if(!file.exists(`${config.booterName}.txt`)){
+    fs.writeFile(`${config.booterName}.txt`, "\n", function(err) {
+        if(err) {
+            console.log(`Could not create temporary IP storage file. ${err}`);
+            process.exit(1);
+        }
+    });
+}
+
+
+
+tail.on("line", function(data){
+    ipAddr = data.split(" ")[0];
+    if(!recordedips.includes(ipAddr)){
+        recordedips.push(ipAddr);
+        fs.appendFile(`${config.booterName}.txt`, `${ipAddr}\n`, function(err) {
+            if(err) {
+                console.log(err);
+                process.exit(1);
+            }
+            console.log(`${ipAddr} detected.`);
+        });
+    }
+})
